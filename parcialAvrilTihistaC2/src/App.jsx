54dfb933c948b29619ea5ea1ca577b7443b4b655
avril-tihista <imaviiii22@gmail.com>
2024-08-14 20:59:32 -0300

    Primer commit

---
+++ parcialAvrilTihistaC2/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Form from './Components/Form'
+import Card from './Components/Card'
+
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>Carga de estudiantes</h1>
+      <Form />
+      <Card />
+    </>
+  )
+}
+
+export default App
